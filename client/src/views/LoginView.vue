224a997735e692985a319511d40ff1fcb1cdd1cb
AndanaSat <113823539+AndanaSat@users.noreply.github.com>
2023-03-27 17:08:02 +0700

    Update LoginView.vue

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -10,19 +10,44 @@ import TheWelcome from '../components/TheWelcome.vue'
     <p>Password</p>
     <input type="text" v-model="password">
     <button>login</button>
+    <p> {{ validation }} </p>
   </main>
 </template>
 
 <script>
+    import axios from "axios"
     export default {
         data() {
             return{
                 email: "",
-                password: ""
+                password: "",
+                validation: ""
             }
         },
         methods:{
-
+          async postUser(){
+            let res = await axios.post('http://localhost:8000/api/login', {
+              email: this.email,
+              password: this.password
+            })
+            .then((response) => {
+              console.log;
+              if(
+                response.data == "auth/invalid-email" ||
+                response.data == "auth/missing-email" ||
+                response.data == "auth/wrong-password" ||
+                response.data == "auth/user-not-found"
+              ){
+                this.validation = "wrong email or password";
+              }
+              else{
+                const uid = response.data;
+                console.log(uid);
+                localStorage.setItem('uid', uid);
+                this.$router.push({path: "/"})
+              }
+            })
+          }
         }
     }
 </script>
